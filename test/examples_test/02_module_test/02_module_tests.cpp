f27d1a91c2c172c2356820e359ef3a68786907f3
Takao <takao.matsuyama@g.austincc.edu>
2020-02-05 18:23:54 -0600

    This tells Catch to provide a main()

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -1,7 +1,34 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "if.h"
+#include "if_else.h"
+#include "switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test is even function")
+{
+	REQUIRE(is_even(2) == true)
+	REQUIRE(is_even(3) == false)
+}
+
+TEST_CASE("Test get generation function")
+
+	REQUIRE(get_genaration(1997) == "Centenial");
+	REQUIRE(get_genaration(1995) == "Millenial");
+	REQUIRE(get_genaration(1970) == "Generation X");
+	REQUIRE(get_genaration(1950) == "Baby Boomer");
+	REQUIRE(get_genaration(1930) == "Silent Generation");
+}
+
+TEST_CASE("Test menu function")
+{
+	REQUIRE(menu(1) == "Option 1");
+	REQUIRE(menu(2) == "Option 2");
+	REQUIRE(menu(3) == "Option 3");
+	REQUIRE(menu(4) == "Option 4");
+	REQUIRE(menu(0) == "Invalid Option");
+	REQUIRE(munu(5) == "Invalid Option");
+}
